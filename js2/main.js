03732d515545a8b8122ea18a2edd5f2f64190b44
ronhm <117081731+ronhm@users.noreply.github.com>
2022-11-11 15:44:18 +0700

    Add files via upload

---
+++ js2/main.js
@@ -0,0 +1,26 @@
+
+function fruitSearch() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("app_list");
+    li = ul.getElementsByTagName('li');
+
+    if(input.value.length == 0){
+        ul.style.display = "none";
+        return;
+    }else{
+        ul.style.display = "block";
+    }
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "block";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+	
