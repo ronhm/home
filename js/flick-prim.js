06e5f4f272e8915216b5fb53d31c7fb153ac90c6
ronhm <117081731+ronhm@users.noreply.github.com>
2023-02-08 17:55:17 +0700

    Add files via upload

---
+++ js/flick-prim.js
@@ -0,0 +1,28 @@
+function FLICK(LINK) {
+	tede = ["Downloading files",]
+	var progress = 0;
+	var dialog = app.dialog.progress('Processing...', progress);
+	dialog.setText("Checking compatibility with your device");
+	var interval = setInterval(function() {
+		progress += 1;
+		dialog.setProgress(progress);
+		if (progress > 20) {
+			dialog.setText(tede[0]);
+		}
+		if (progress > 35) {
+			dialog.setText(tede[1]);
+		}
+		if (progress > 60) {
+			dialog.setText(tede[2]);
+		}
+		if (progress > 80) {
+			dialog.setText(tede[3]);
+		}	
+		if (progress === 100) {
+			clearInterval(interval);
+			setTimeout(function() {
+				window.location.href = LINK;
+			}, 2500)
+		}
+	}, 150)
+}
