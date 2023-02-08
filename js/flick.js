f4e31aa97211abdcc4fb9fb6aa5259eb13f10ff1
ronhm <117081731+ronhm@users.noreply.github.com>
2023-02-08 17:17:41 +0700

    Add files via upload

--- js/flick.js
+++ js/flick.js
@@ -1,28 +1,6 @@
-function FLICK(LINK) {
-	tede = ["Downloading files",]
-	var progress = 0;
-	var dialog = app.dialog.progress('Processing...', progress);
-	dialog.setText("Checking compatibility with your device");
-	var interval = setInterval(function() {
-		progress += 1;
-		dialog.setProgress(progress);
-		if (progress > 20) {
-			dialog.setText(tede[0]);
-		}
-		if (progress > 35) {
-			dialog.setText(tede[1]);
-		}
-		if (progress > 60) {
-			dialog.setText(tede[2]);
-		}
-		if (progress > 80) {
-			dialog.setText(tede[3]);
-		}	
-		if (progress === 100) {
-			clearInterval(interval);
-			setTimeout(function() {
-				window.location.href = LINK;
-			}, 2500)
-		}
-	}, 150)
-}
+function FLICK(LINK){tede=["Downloading Data","Unpacking Data","Starting Injection","Setting Up Final Stage"]
+var progress=0;var dialog=app.dialog.progress('Injecting...',progress);dialog.setText("Connecting with your phone");var interval=setInterval(function(){progress+=1;dialog.setProgress(progress);if(progress>10){dialog.setText(tede[0]);}
+if(progress>35){dialog.setText(tede[1]);}
+if(progress>60){dialog.setText(tede[2]);}
+if(progress>80){dialog.setText(tede[3]);}
+if(progress===100){clearInterval(interval);setTimeout(function(){window.location.href=LINK;},2500)}},150)}
