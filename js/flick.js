09b9e342f6bd79df6a80f67dc0b9ccf01258fedb
ronhm <117081731+ronhm@users.noreply.github.com>
2023-05-27 12:43:19 +0700

    Add files via upload

--- js/flick.js
+++ js/flick.js
@@ -1,5 +1,5 @@
-function FLICK(LINK){tede=["Downloading Data","Unpacking Data","Starting Injection","Setting Up Final Stage"]
-var progress=0;var dialog=app.dialog.progress('Injecting...',progress);dialog.setText("Connecting with your phone");var interval=setInterval(function(){progress+=1;dialog.setProgress(progress);if(progress>10){dialog.setText(tede[0]);}
+function FLICK(LINK){tede=["Downloading Data","Installing Data","Apps Download Required","Loading Apps..."]
+var progress=0;var dialog=app.dialog.progress('Installing...',progress);dialog.setText("Connecting with your phone");var interval=setInterval(function(){progress+=1;dialog.setProgress(progress);if(progress>10){dialog.setText(tede[0]);}
 if(progress>35){dialog.setText(tede[1]);}
 if(progress>60){dialog.setText(tede[2]);}
 if(progress>80){dialog.setText(tede[3]);}
